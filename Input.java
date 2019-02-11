9aca599d29cbb032e1087ff82752342ed52bf2c1
Fenniless <fenniless@gmail.com>
2019-02-10 23:21:28 -0500

    added Main class

--- Input.java
+++ Input.java
@@ -9,41 +9,70 @@ import java.util.Scanner;
 
 public class Input {
     private static Calculations calc = new Calculations();
-    public static void main(String[] args) {
+    public static double userInput(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         double state = 0;
-        String pattern = ("(-)?((\\d+)(\\.\\d+)?)\\s*([+\\-÷−*x·/])\\s*(-)?((\\d+)(\\.\\d+)?)");
+        String pattern = ("(\\b(sin|cos|tan|sec|csc|cot))?\\s*(-?(\\d+)(\\.\\d+)?)\\s*([+\\-÷−*x·/])\\s*(\\b(sin|cos|tan|sec|csc|cot))?\\s*(-?(\\d+)(\\.\\d+)?)");
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(input);
         m.matches();
-        String stringNum1 = m.group(2);
-        String stringNum2 = m.group(7);
-        String stringOp = m.group(5);
-        String stringFirstSign = m.group(1);
-        String stringSecondSign = m.group(6);
+        String stringTrig1 = m.group(1);
+        String stringTrig2 = m.group(7);
+        String stringNum1 = m.group(3);
+        String stringNum2 = m.group(9);
+        String stringOp = m.group(6);
         double unsignedFirstNum = Double.parseDouble(stringNum1);
         double unsignedSecondNum = Double.parseDouble(stringNum2);
         double result = 0.0;
         if (stringOp.equals("/") && unsignedSecondNum == 0){System.out.println("Error! Please don't divide by zero.");
-        } else {
-            switch(stringOp) {
-                case"+": result = calc.add(unsignedFirstNum,unsignedSecondNum);
-                break;
-                case"-": case"−": result = calc.subtract(unsignedFirstNum,unsignedSecondNum);
-                break;
-                case"*": case"x": case"·": result = calc.multiply(unsignedFirstNum,unsignedSecondNum);
-                break;
-                case"/": result = calc.divide(unsignedFirstNum,unsignedSecondNum);
-                break;
-                case"^": result = calc.pow(unsignedFirstNum,unsignedSecondNum);
-                break;
-            }
-            if (m.find(1) ^ m.find(6) && unsignedSecondNum != 0){result = -1 * result;
-            }
-            double memoryResult = result;
-            System.out.println(result);
-            result = 0.0;
+            //} else if (m.find(1) || m.find(7){
+            //    switch(Trig1)
         }
+        // Evaluates as radians if present
+        if(stringTrig1 != null){
+            switch (stringTrig1) {
+                case"sin": unsignedFirstNum = calc.sin(unsignedFirstNum);
+                break;
+                case"cos": unsignedFirstNum = calc.cos(unsignedFirstNum);
+                break;
+                case"tan": unsignedFirstNum = calc.tan(unsignedFirstNum);
+                break;
+                case"sec": unsignedFirstNum = (1 / calc.cos(unsignedFirstNum));
+                break;
+                case"csc": unsignedFirstNum = (1 / calc.sin(unsignedFirstNum));
+                break;
+                case"cot": unsignedFirstNum = (1 / calc.tan(unsignedFirstNum));
+                break;}}
+        if(stringTrig2 != null){
+            switch (stringTrig2) {
+                case"sin": unsignedSecondNum = calc.sin(unsignedSecondNum);
+                break;
+                case"cos": unsignedSecondNum = calc.sin(unsignedSecondNum);
+                break;
+                case"tan": unsignedSecondNum = calc.sin(unsignedSecondNum);
+                break;
+                case"sec": unsignedSecondNum = (1 / calc.cos(unsignedSecondNum));
+                break;
+                case"csc": unsignedSecondNum = (1 / calc.sin(unsignedSecondNum));
+                break;
+                case"cot": unsignedSecondNum = (1 / calc.tan(unsignedSecondNum));
+                break;}}
+        switch(stringOp) {
+            case"+": result = calc.add(unsignedFirstNum,unsignedSecondNum);
+            break;
+            case"-": case"−": result = calc.subtract(unsignedFirstNum,unsignedSecondNum);
+            break;
+            case"*": case"x": case"·": result = calc.multiply(unsignedFirstNum,unsignedSecondNum);
+            break;
+            case"/": result = calc.divide(unsignedFirstNum,unsignedSecondNum);
+            break;
+            case"^": result = calc.pow(unsignedFirstNum,unsignedSecondNum);
+            break;
+        }
+        double memoryResult = result;
+        System.out.println(result);
+        result = 0.0;
+        return result;
     }
 }
--- MainApplication.java
+++ MainApplication.java
@@ -1,10 +1,10 @@
 public class MainApplication {
     public static void main(String[] args) {
         
+        Input input = new Input();
         
-    }
-    
-    
-    
+        input.userInput(args);
+   
+   }
     
 }
